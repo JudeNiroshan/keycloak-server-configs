3c1fddcd4245dfdb4e6b8b91479f5324a18aa7cf
jude <jude.niroshan11@gmail.com>
2023-05-25 23:57:25 +0200

    Init commit

---
+++ post-commit.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Define the folder path
+data_folder="./data"
+
+# Retrieve the content of the root YAML file
+root_yaml_file="$data_folder/my-realm.yaml"
+combined="./combined.yaml"
+
+cat "$root_yaml_file" > $combined
+
+# Define the subdirectories and their corresponding attributes
+subdirectories=(
+  "clients"
+  "groups"
+  "identityProviders"
+)
+
+# Iterate over the subdirectories and their attributes
+for ((i=0; i<${#subdirectories[@]}; i+=2)); do
+  subdirectory="${subdirectories[i]}"
+
+  # Combine the content of each file in the subdirectory
+  for file in "$data_folder/$subdirectory"/*; do
+    file_content=$(cat "$file")
+
+    new=$(yq ea '
+    (
+      (
+        select(fi==0) ) as $item ireduce([]; . + $item
+      )
+    ) as $ma
+    | select(fi == 1) | .'$subdirectory' += $ma ' $file $combined)
+
+    echo "$new" > $combined
+  done
+done
